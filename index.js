60fee959a87964db0708a63f73c7861de4fac221
Jason Walker <jlwalker@r12mail.sps.org>
2024-05-20 16:15:01 -0500

    add libraries

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import { extension_settings, getContext, loadExtensionSettings } from "../../../
 //You'll likely need to import some other functions from the main script
 import { saveSettingsDebounced } from "../../../../script.js";
 
+import { NovelAiApi } from "lib/novel-ai-api/novel-ai-api.js";
+
 // Keep track of where your extension is located, name should match repo name
 const extensionName = "st-extension-novelai-import";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
